41c853cfa14392e0eb10b966cb6802ef8339b872
Matt Vitelli <matthew.vitelli@gmail.com>
2012-07-18 11:13:04 -0700

    Got smd animations importing correctly. Now its time to make them play correctly

---
+++ Gaia/Core/Tokenizer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Gaia.Core
+{
+    public class Tokenizer
+    {
+        Queue<string> tokens = new Queue<string>();
+
+        public Tokenizer(string text)
+        {
+            SetInput(text);
+        }
+
+        public Tokenizer(StreamReader reader)
+        {
+            SetInput(reader);
+        }
+
+        public bool HasMoreTokens()
+        {
+            return (tokens.Count > 0);
+        }
+
+        public void SetInput(string text)
+        {
+            tokens.Clear();
+            char[] separators = new char[] {' ', '\r', '\n' };
+            string[] strings = text.Split(separators);
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i] != string.Empty && strings[i] != "")
+                    tokens.Enqueue(strings[i]);
+            }
+        }
+
+        public void SetInput(StreamReader reader)
+        {
+            SetInput(reader.ReadToEnd());
+        }
+
+        public string GetNextToken()
+        {
+            return tokens.Dequeue();
+        }
+
+        public string Peek()
+        {
+            return tokens.Peek();
+        }
+    }
+}
--- Gaia/Resources/AnimationSequence.cs
+++ Gaia/Resources/AnimationSequence.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Xml;
+using System.IO;
 using Microsoft.Xna.Framework;
+using Gaia.Core;
 
 namespace Gaia.Resources
 {
@@ -43,6 +45,7 @@ namespace Gaia.Resources
 
         void IResource.LoadFromXML(XmlNode node)
         {
+            string filename = string.Empty;
             foreach (XmlAttribute attrib in node.Attributes)
             {
                 switch (attrib.Name.ToLower())
@@ -59,15 +62,136 @@ namespace Gaia.Resources
                     case "mesh":
                         mesh = ResourceManager.Inst.GetMesh(attrib.Value);
                         break;
+                    case "filename":
+                        filename = attrib.Value;
+                        break;
                     case "name":
                         name = attrib.Value;
                         break;
                 }
             }
-            mesh.ExtractNodesBetweenFrames(timeStart, timeEnd, out positionFrames, out rotationFrames);
+            if (filename != string.Empty)
+            {
+                timeStart = 0;
+                ReadSMD(filename);
+            }
+            else
+            {
+                mesh.ExtractNodesBetweenFrames(timeStart, timeEnd, out positionFrames, out rotationFrames);
+            }
             RescaleAnimation(positionFrames, timeStart, fps);
             RescaleAnimation(rotationFrames, timeStart, fps);
             endTime = (timeEnd - timeStart) / fps;
+        }
+
+        void ReadNodes(Tokenizer input, SortedList<int, string> nodesToNames)
+        {
+            string text = input.GetNextToken();
+            while (text != "nodes")
+                text = input.GetNextToken();
+
+            while (input.Peek() != "end")
+            {
+                int id = int.Parse(input.GetNextToken());
+                string name = input.GetNextToken();
+                name = name.Substring(1, name.Length - 2);
+                nodesToNames.Add(id, name);
+                input.GetNextToken(); // the parent index (not important)
+            }
+            input.GetNextToken(); //read over the end
+        }
+
+        void ReadSkeleton(Tokenizer input, SortedList<int, List<ModelBoneAnimationFrame>> posFrames, SortedList<int, List<ModelBoneAnimationFrame>> rotFrames)
+        {
+            string text = input.GetNextToken();
+            while (text != "skeleton")
+                text = input.GetNextToken();
+            int currTime = 0;
+            timeStart = currTime;
+            while (input.Peek() != "end")
+            {
+                text = input.GetNextToken();
+                if (text == "time")
+                {
+                    currTime = int.Parse(input.GetNextToken());
+                }
+                else
+                {
+                    int boneID = int.Parse(text);
+                    Vector3 posDisp;
+                    posDisp.X = float.Parse(input.GetNextToken());
+                    posDisp.Y = float.Parse(input.GetNextToken());
+                    posDisp.Z = float.Parse(input.GetNextToken());
+                    Vector3 rotDisp;
+                    rotDisp.X = float.Parse(input.GetNextToken());
+                    rotDisp.Y = float.Parse(input.GetNextToken());
+                    rotDisp.Z = float.Parse(input.GetNextToken());
+
+                    ModelBoneAnimationFrame posFrame = new ModelBoneAnimationFrame();
+                    posFrame.Displacement = posDisp;
+                    posFrame.time = currTime;
+
+                    ModelBoneAnimationFrame rotFrame = new ModelBoneAnimationFrame();
+                    rotFrame.Displacement = rotDisp;
+                    rotFrame.time = currTime;
+
+                    if (!posFrames.ContainsKey(boneID))
+                        posFrames.Add(boneID, new List<ModelBoneAnimationFrame>());
+                    if (!rotFrames.ContainsKey(boneID))
+                        rotFrames.Add(boneID, new List<ModelBoneAnimationFrame>());
+
+                    posFrames[boneID].Add(posFrame);
+                    rotFrames[boneID].Add(rotFrame);
+                }
+            }
+            timeEnd = currTime;
+            fps = 24;
+        }
+
+        void ReadSMD(string filename)
+        {
+            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            {
+                using (StreamReader reader = new StreamReader(fs))
+                {
+                    Tokenizer tokenizer = new Tokenizer(reader);
+                    SortedList<int, string> nodesToNames = new SortedList<int,string>();
+                    ReadNodes(tokenizer, nodesToNames);
+
+                    SortedList<int, List<ModelBoneAnimationFrame>> posFrames = new SortedList<int,List<ModelBoneAnimationFrame>>();
+                    SortedList<int, List<ModelBoneAnimationFrame>> rotFrames = new SortedList<int,List<ModelBoneAnimationFrame>>();
+                    ReadSkeleton(tokenizer, posFrames, rotFrames);
+
+                    positionFrames = new SortedList<string, ModelBoneAnimationFrame[]>();
+                    for (int i = 0; i < posFrames.Keys.Count; i++)
+                    {
+                        int currKey = posFrames.Keys[i];
+                        string currName = nodesToNames[currKey];
+                        positionFrames.Add(currName, posFrames[currKey].ToArray());
+                        for (int j = 0; j < positionFrames[currName].Length; j++)
+                        {
+                            positionFrames[currName][j].boneName = currName;
+                        }
+                    }
+
+                    rotationFrames = new SortedList<string, ModelBoneAnimationFrame[]>();
+                    for (int i = 0; i < rotFrames.Keys.Count; i++)
+                    {
+                        int currKey = rotFrames.Keys[i];
+                        string currName = nodesToNames[currKey];
+                        rotationFrames.Add(currName, rotFrames[currKey].ToArray());
+                        for (int j = 0; j < rotationFrames[currName].Length; j++)
+                        {
+                            rotationFrames[currName][j].boneName = currName;
+                        }
+                    }
+                }
+            }
+        }
+
+        void ReadAnimationFrames(StreamReader reader)
+        {
+            string text = reader.ReadLine();
         }
 
         void RescaleAnimation(SortedList<string, ModelBoneAnimationFrame[]> frames, float startTime, float fps)
